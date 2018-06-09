c738480b9c752dbbac936b25e3c5d2a782366628
Oleksandr Shchetynin <justrocknrolla@gmail.com>
2018-06-09 19:59:26 +0300

    scatter

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Eos from 'eosjs';
 
 class App extends Component {
     state = {};
+
     constructor(props) {
         super(props);
         this.eos = Eos();
@@ -15,25 +16,44 @@ class App extends Component {
             this.eos.getInfo({}).then(res => {
                 this.setState({info: res})
             });
-        },300)
+        }, 1000);
     }
-  render() {
-      let info = this.state.info;
-      return info ? (
-      <div className="App">
-        <div className="App-intro" style={{padding:10}}>
-            <h3>EOS</h3>
-            <table>
-                <tbody>
-                {Object.keys(info).map(k =>
-                    <tr align="left" key={k}><td>{k}</td><td>{info[k]}</td></tr>
-                )}
-                </tbody>
-            </table>
-        </div>
-      </div>
-    ) : null;
-  }
+
+    componentDidMount() {
+        this.window = window;
+    }
+
+    getIdentity = () => {
+        this.window.scatter.getIdentity().then(identity => {
+            console.info('Identity: ' + identity)
+            alert('Identity: ' + identity.name);
+        }).catch(error => {
+            console.error(error)
+            alert('Error: ' + error.message);
+        });
+    };
+
+    render() {
+        const info = this.state.info;
+        return info ? (
+            <div className="App">
+                <div className="App-intro" style={{padding: 10}}>
+                    <h3>EOS</h3>
+                    <table>
+                        <tbody>
+                        {Object.keys(info).map(k =>
+                            <tr align="left" key={k}>
+                                <td>{k}</td>
+                                <td>{info[k]}</td>
+                            </tr>
+                        )}
+                        </tbody>
+                    </table>
+                    <button type={'button'} onClick={this.getIdentity}>Get Identity</button>
+                </div>
+            </div>
+        ) : null;
+    }
 }
 
 export default App;
