7daec66d8f029c30f80049be864efcea9e5d0fe8
Oleksandr Shchetynin <justrocknrolla@gmail.com>
2018-06-09 20:37:22 +0300

    some markup

---
+++ reactapp/src/BuyerPage.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class BuyerPage extends Component {
+    render() {
+        return 'Buyer'
+    }
+}
+
+export default BuyerPage;
---
+++ reactapp/src/CoderPage.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class CoderPage extends Component {
+    render() {
+        return 'Coder'
+    }
+}
+
+export default CoderPage;
